4cc8f59c23998c7c4ab8ca2a61e7b9c784da32ea
MARIANO CACERES SMOLER <46956818@ort>
2022-06-30 14:51:00 -0300

    Sala de Escape

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
-namespace SalaDeEscape_BuscandoVolver.Models;
+using System;
 
-public class ErrorViewModel
+namespace SalaEscape.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
